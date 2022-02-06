e2d95dfc400f821527d5d759b53727cab94ff1a3
Jooyoung Oh <jooyoungoh.03@gmail.com>
2022-02-06 20:00:51 +0300

    Initial Commit

---
+++ init_db.py
@@ -0,0 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.localDB
+
+
+# Import source url of movie stars to be store in DB
+def get_urls():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('https://movie.naver.com/movie/sdb/rank/rpeople.nhn', headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    trs = soup.select('#old_content > table > tbody > tr')
+
+    urls = []
+    for tr in trs:
+        a = tr.select_one('td.title > a')
+        if a is not None:
+            base_url = 'https://movie.naver.com/'
+            url = base_url + a['href']
+            urls.append(url)
+
+    return urls
+
+
+# Import photos, names, and information from the url and save in 'mystar' collection
+def insert_star(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    name = soup.select_one('#content > div.article > div.mv_info_area > div.mv_info.character > h3 > a').text
+    img_url = soup.select_one('#content > div.article > div.mv_info_area > div.poster > img')['src']
+    recent_work = soup.select_one(
+        '#content > div.article > div.mv_info_area > div.mv_info.character > dl > dd > a:nth-child(1)').text
+
+    doc = {
+        'name': name,
+        'img_url': img_url,
+        'recent': recent_work,
+        'url': url,
+        'like': 0
+    }
+
+    db.mystar.insert_one(doc)
+    print('Complete!', name)
+
+
+# Delete existing data and repeat the process
+def insert_all():
+    db.mystar.drop()  # delete 'mystar' collection
+    urls = get_urls()
+    for url in urls:
+        insert_star(url)
+
+
+### Execute
+insert_all()
